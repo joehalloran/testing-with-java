625d24e5cb0aac707408d50431902e968c7ed07b
joehalloran <joe_halloran@hotmail.co.uk>
2017-06-07 12:12:46 +0100

    Added setUp method to FactorialTest

--- step-1-junit/test/FactorialTest.java
+++ step-1-junit/test/FactorialTest.java
@@ -1,40 +1,42 @@
+
 import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 import org.junit.Test;
 
 public class FactorialTest {
+
+    private Factorial factorial;
+
+    @Before
+    public void setUp() {
+        factorial = new Factorial();
+    }
+
     @Test
     public void calculate_0() {
-        Factorial factorial = new Factorial();
         int testStartValue = 0;
         int expectedResult = 0;
-
         assertEquals(expectedResult, factorial.calculate(testStartValue));
     }
 
     @Test
     public void calculate_1() {
-        Factorial factorial = new Factorial();
         int testStartValue = 1;
         int expectedResult = 1;
-
         assertEquals(expectedResult, factorial.calculate(testStartValue));
     }
 
     @Test
     public void calculate_2() {
-        Factorial factorial = new Factorial();
         int testStartValue = 2;
         int expectedResult = 2;
-
         assertEquals(expectedResult, factorial.calculate(testStartValue));
     }
 
     @Test
     public void calculate_3() {
-        Factorial factorial = new Factorial();
         int testStartValue = 3;
         int expectedResult = 6;
-
         assertEquals(expectedResult, factorial.calculate(testStartValue));
     }
 
