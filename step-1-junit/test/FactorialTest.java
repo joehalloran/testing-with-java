83e0523bdbbe01fa879a92e1a0e3213b93bb71be
joehalloran <joe_halloran@hotmail.co.uk>
2017-06-07 11:27:27 +0100

    3 additional tests for Factorial

--- step-1-junit/test/FactorialTest.java
+++ step-1-junit/test/FactorialTest.java
@@ -10,4 +10,31 @@ public class FactorialTest {
 
         assertEquals(expectedResult, factorial.calculate(testStartValue));
     }
+
+    @Test
+    public void calculate_1() {
+        Factorial factorial = new Factorial();
+        int testStartValue = 1;
+        int expectedResult = 1;
+
+        assertEquals(expectedResult, factorial.calculate(testStartValue));
+    }
+
+    @Test
+    public void calculate_2() {
+        Factorial factorial = new Factorial();
+        int testStartValue = 2;
+        int expectedResult = 2;
+
+        assertEquals(expectedResult, factorial.calculate(testStartValue));
+    }
+
+    @Test
+    public void calculate_3() {
+        Factorial factorial = new Factorial();
+        int testStartValue = 3;
+        int expectedResult = 6;
+
+        assertEquals(expectedResult, factorial.calculate(testStartValue));
+    }
 }
