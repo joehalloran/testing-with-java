79ddd07a1ca88b93d78a8e3e30de5fd676f5ef98
joehalloran <joe_halloran@hotmail.co.uk>
2017-06-08 10:56:19 +0100

    Fibonacci testing

--- step-2-tdd-fibonacci-junit/src/Fibonacci.java
+++ step-2-tdd-fibonacci-junit/src/Fibonacci.java
@@ -1,2 +1,15 @@
 public class Fibonacci {
+
+    // Finds the nth item in the fibonacci sequence
+    public int fib(int n){
+        if (n < 0) {
+            throw new IllegalArgumentException("Factorials are defined only on non-negative integers.");
+        }
+
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        return fib(n - 1) + fib(n - 2);
+    }
 }
--- step-2-tdd-fibonacci-junit/test/FibonacciTest.java
+++ step-2-tdd-fibonacci-junit/test/FibonacciTest.java
@@ -1,2 +1,33 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
 public class FibonacciTest {
+
+    private Fibonacci fibonacci;
+
+    private int[] results = {1,1,2,3,5,8,13};
+
+    @Before
+    public void setUp(){
+        fibonacci = new Fibonacci();
+    }
+
+    @Test
+    public void fib_0(){
+        assertEquals(1, fibonacci.fib(0));
+    }
+
+    @Test
+    public void fib_6(){
+        assertEquals(13, fibonacci.fib(6));
+    }
+
+    @Test
+    public void fib_test(){
+        for(int i = 0; i < results.length; i++) {
+            assertEquals(results[i], fibonacci.fib(i));
+        }
+    }
+
 }
