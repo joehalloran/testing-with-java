02d650bf15fb3e8350757b27d99c42e09327a846
joehalloran <joe_halloran@hotmail.co.uk>
2017-06-09 16:25:24 +0100

    Step 3 lesson - creating pom.xml

---
+++ step-3-maven-fibonacci/src/main/java/com/mattcalthrop/examples/factorial/Factorial.java
@@ -0,0 +1,17 @@
+package com.mattcalthrop.examples.factorial;
+
+public class Factorial {
+    public int calculate(int start) {
+        if (start < 0) {
+            throw new IllegalArgumentException("Factorials are defined only on non-negative integers.");
+        }
+
+        int result = start;
+
+        if (result > 1) {
+            result = start * calculate(start - 1);
+        }
+
+        return result;
+    }
+}
--- step-3-maven-fibonacci/src/test/java/com/mattcalthrop/examples/factorial/FactorialTest.java
+++ step-3-maven-fibonacci/src/test/java/com/mattcalthrop/examples/factorial/FactorialTest.java
@@ -1,3 +1,4 @@
+package com.mattcalthrop.examples.factorial;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
